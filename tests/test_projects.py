e90b49c5840a8a71703817d0d052069bcabd15b3
Esteban Vallejo <estebanfullmusic@gmail.com>
2026-08-08 22:15:02 -0500

    Validation from none existing project Test

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -6,3 +6,9 @@ client = TestClient(app)
 def test_get_project_by_id():
     response = client.get("/projects/1")
     assert response.status_code == 200
+    assert response.json()["id"] == 1
+
+def test_nonexistent_project():
+    response = client.get("/projects/999")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Project with ID 999 not found!"
